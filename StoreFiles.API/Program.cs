8b8fbe21c9c5c84760c5b8d84083511a16b26c31
Danbeac <danbeac@hotmail.com>
2022-11-01 12:06:37 -0500

    Se modifica el API de StoreFiles para que permita ser configurable el puerto por el que se ejecuta la app

--- StoreFiles.API/Program.cs
+++ StoreFiles.API/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,11 +17,26 @@ namespace StoreFiles.API
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+
+            var host = new WebHostBuilder();
+            var env = host.GetSetting("environment");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls(configuration["hostUrl"]);
+
                 });
+        }
     }
 }
