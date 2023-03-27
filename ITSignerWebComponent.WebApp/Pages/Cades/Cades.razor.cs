1d89d7f57d8a2e52868a9ee94cd6eee18251a969
Danbeac <danbeac@hotmail.com>
2023-03-27 15:47:41 -0500

    Se modifica pagina de firma cades habilitando la firma del archivo con certificados locales atravez de Fortify

--- ITSignerWebComponent.WebApp/Pages/Cades/Cades.razor.cs
+++ ITSignerWebComponent.WebApp/Pages/Cades/Cades.razor.cs
@@ -1,11 +1,15 @@
 ﻿using CurrieTechnologies.Razor.SweetAlert2;
 using ITSignerWebComponent.SignApp.APIStoreFiles;
+using ITSignerWebComponent.SignApp.Error;
 using ITSignerWebComponent.SignApp.Responses.APIStoreResponses;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Configuration;
 using Microsoft.JSInterop;
 using StoreFiles.Core.DTOs.Cades;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ITSignerWebComponent.SignApp.Pages.Cades
 {
@@ -26,6 +30,73 @@ namespace ITSignerWebComponent.SignApp.Pages.Cades
 
         public CadesFileResponse ResponseService { get; private set; }
         public bool SignedFile { get; private set; }
+        public string[] ValidCAs { get; set; }
+        [Inject]
+        private IConfiguration configuration { get; set; }
+        [Inject]
+        public IErrorService _errorService { get; set; }
+        [Inject]
+        private NavigationManager UriHelper { get; set; }
+
+        protected override async void OnInitialized()
+        {
+            LoadValidCertificatesAuthorities();
+            await _jsRuntime.InvokeVoidAsync("cleanLocalStorage");
+        }
+
+        public async void onSignCadesFileV2()
+        {
+            if (this.Base64Doc is null)
+            {
+                await _swalService.FireAsync("Error", "Debe seleccionar el archivo a firmar", "error");
+            }
+            else
+            {
+                string pem = await _jsRuntime.InvokeAsync<string>("getPemSelected");
+                if (!string.IsNullOrEmpty(pem))
+                {
+                    try
+                    {
+
+                        if (await isValidCertificateAuthority())
+                        {
+                            await _jsRuntime.InvokeVoidAsync("setLoadingSigningButton");
+
+                            CadesFileDto.File = this.Base64Doc;
+
+                            var cms = await _jsRuntime.InvokeAsync<string>("generateCMSToEmbed", this.Base64Doc);
+
+                            SignedFile = true;
+
+                            await _jsRuntime.InvokeVoidAsync("downloadFileP7z", cms, Filename);
+                            await _swalService.FireAsync("Exitoso", "¡ Archivo Firmado y Descargado Correctamente !", "success");
+                            ReloadPage();
+                        }
+                        else
+                        {
+                            await _swalService.FireAsync("Error", "El certificado seleccionado no cuenta con una Autoridad de certificación válida", "error");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.Message.Contains("Client cipher is not initialized") || ex.Message.Contains("Socket connection is not open"))
+                        {
+                            onSignCadesFileV2();
+                        }
+                        else
+                        {
+                            _errorService.ProcessError(ex);
+                            await _jsRuntime.InvokeVoidAsync("cleanLocalStorage");
+                        }
+                    }
+                }
+                else
+                {
+                    await _swalService.FireAsync("Error", "Por favor seleccione el certificado y a continuación de click sobre el botón Continuar", "error");
+                }
+            }
+                
+        }
 
         public async void onSignCadesFile()
         {
@@ -75,7 +146,38 @@ namespace ITSignerWebComponent.SignApp.Pages.Cades
 
                 this.Base64Doc = buffer;
             }
-
         }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await _jsRuntime.InvokeVoidAsync("InitializeFortify");
+            await _jsRuntime.InvokeVoidAsync("enableFormSign");
+        }
+        private async Task<bool> isValidCertificateAuthority()
+        {
+            bool isValid = false;
+
+            foreach (var authority in this.ValidCAs)
+            {
+                if (await _jsRuntime.InvokeAsync<bool>("isValidAuthorityCertificate", authority))
+                {
+                    return true;
+                }
+            }
+
+            return isValid;
+        }
+
+        private void LoadValidCertificatesAuthorities()
+        {
+            this.ValidCAs = configuration.GetSection("CertificatesAuthorities").Get<string[]>();
+        }
+
+        private async void ReloadPage()
+        {
+            await _jsRuntime.InvokeVoidAsync("cleanLocalStorage");
+            UriHelper.NavigateTo(UriHelper.Uri, forceLoad: true);
+        }
+
     }
 }
